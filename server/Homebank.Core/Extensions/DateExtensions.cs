0ab1c59fb47cfabf5be68df23f18fe345ca34b4f
Timo <timo.hermans@outlook.com>
2019-04-22 21:58:53 +0200

    Make sure to not DRY for month checks

---
+++ server/Homebank.Core/Extensions/DateExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homebank.Core.Extensions
+{
+    public static class DateExtensions
+    {
+        public static bool IsSameMonthAndYear(this DateTime date, DateTime dateToCheck)
+        {
+            return date.Year == dateToCheck.Year && date.Month == dateToCheck.Month;
+        }
+    }
+}
