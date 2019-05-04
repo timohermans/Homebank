558f5e535692676e5bcca717ca2581f93c6d6af8
Timo <timo.hermans@outlook.com>
2019-05-04 17:11:07 +0200

    Make sure the month picker pops up when clicked on

--- client/homebank-client/src/budgets/budget-header/BudgetHeader.tsx
+++ client/homebank-client/src/budgets/budget-header/BudgetHeader.tsx
@@ -1,23 +1,29 @@
 import * as React from 'react';
-import './BudgetHeader.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { MonthPicker } from './month-picker/MonthPicker';
+import './BudgetHeader.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {MonthPicker} from './month-picker/MonthPicker';
 
 export class BudgetHeader extends React.Component {
-    render() {
-        return <nav className="navbar bg-header navbar-dark">
+  render() {
+    return (
+      <nav className="navbar bg-header navbar-dark">
+        <div className="form-inline flex-grow-1 d-flex justify-content-center">
+          <div className="px-2 icon">
+            <FontAwesomeIcon icon="arrow-alt-circle-left" />
+          </div>
+          <div className="selected-month">
+            APR 2019 <FontAwesomeIcon icon="caret-down" />
+            <MonthPicker year={2019} month={4} />
+          </div>
+          <div className="px-2 icon">
+            <FontAwesomeIcon icon="arrow-alt-circle-right" />
+          </div>
+        </div>
 
-            <div className="form-inline flex-grow-1 d-flex justify-content-center">
-                <div className="px-2 icon"><FontAwesomeIcon icon="arrow-alt-circle-left"></FontAwesomeIcon></div>
-                <div className="selected-month">APR 2019 <FontAwesomeIcon icon="caret-down"></FontAwesomeIcon></div>
-                <div className="px-2 icon"><FontAwesomeIcon icon="arrow-alt-circle-right"></FontAwesomeIcon></div>
-            </div>
+        <div className="flex-grow-1">2</div>
 
-            <MonthPicker year={2019} month={4}></MonthPicker>
-
-            <div className="flex-grow-1">2</div>
-
-            <div className="flex-grow-1">3</div>
-        </nav>;
-    }
+        <div className="flex-grow-1">3</div>
+      </nav>
+    );
+  }
 }
--- client/homebank-client/src/budgets/budget-header/month-picker/MonthPicker.tsx
+++ client/homebank-client/src/budgets/budget-header/month-picker/MonthPicker.tsx
@@ -1,34 +1,114 @@
 import React from 'react';
 import './MonthPicker.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { format } from 'date-fns';
+import {format} from 'date-fns';
+import ReactDOM from 'react-dom';
 
 export interface MonthPickerProps {
   year: number;
   month: number;
 }
 
-export class MonthPicker extends React.Component<MonthPickerProps> {
+interface MonthPickerState {
+  style: React.CSSProperties;
+  isVisible: boolean;
+}
+
+export class MonthPicker extends React.Component<MonthPickerProps, MonthPickerState> {
+  private modalRef: React.RefObject<HTMLDivElement>;
+
+  private get pickerParent(): HTMLElement | null {
+    const pickerNode = ReactDOM.findDOMNode(this);
+    if (!pickerNode) {
+      return null;
+    }
+
+    return pickerNode.parentElement;
+  }
+
+  constructor(props: MonthPickerProps) {
+    super(props);
+
+    this.state = {style: {}, isVisible: false};
+    this.modalRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.listenToParentClick();
+    this.determineModalPosition();
+  }
+
+  componentDidUpdate() {
+    this.determineModalPosition();
+  }
+
+  private listenToParentClick() {
+    if (this.pickerParent) {
+      this.pickerParent.addEventListener('click', (event: MouseEvent) => {
+        if (event.target !== this.pickerParent) {
+          return;
+        }
+
+        this.setState(statePreviously => {
+          return {isVisible: !statePreviously.isVisible};
+        });
+      });
+    }
+  }
+
+  private determineModalPosition() {
+    // TODO: when window resizes, recalculate the position
+    if (this.modalRef.current && this.pickerParent) {
+      const someMargin = 10;
+      const parentPosition = this.pickerParent.getBoundingClientRect();
+      const pickerElement = this.modalRef.current as HTMLDivElement;
+
+      const topToBe = parentPosition.top + this.pickerParent.clientHeight + someMargin;
+      const leftToBe =
+        parentPosition.left + this.pickerParent.clientWidth / 2 - pickerElement.clientWidth / 2;
+
+      if (this.state.style.top !== topToBe || this.state.style.left !== leftToBe) {
+        this.setState({
+          style: {
+            top: topToBe,
+            left: leftToBe,
+          },
+        });
+      }
+    }
+  }
+
   render() {
     const months = this.createMonthsToSelect();
 
-    return (
-      <div className="month-picker bg-light text-dark">
-        <div className="year-row d-flex justify-content-between align-items-center mx-2">
-          <div className="icon">
-            <FontAwesomeIcon icon="arrow-alt-circle-left" />
-          </div>
-          <div className="year">2019</div>
-          <div className="icon">
-            <FontAwesomeIcon icon="arrow-alt-circle-right" />
-          </div>
-        </div>
+    if (this.state.isVisible) {
+      return (
+        <div>
+          <div className="modal-overlay" onClick={() => this.setState({isVisible: false})} />
+          <div
+            ref={this.modalRef}
+            className="month-picker bg-light text-dark"
+            style={this.state.style}
+          >
+            <div className="year-row d-flex justify-content-between align-items-center mx-2">
+              <div className="icon">
+                <FontAwesomeIcon icon="arrow-alt-circle-left" />
+              </div>
+              <div className="year">2019</div>
+              <div className="icon">
+                <FontAwesomeIcon icon="arrow-alt-circle-right" />
+              </div>
+            </div>
 
-        <div className="month-container d-flex flex-wrap align-items-center justify-content-center mx-2 my-1">
-          {months}
+            <div className="month-container d-flex flex-wrap align-items-center justify-content-center mx-2 my-1">
+              {months}
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return <div />;
+    }
   }
 
   private createMonthsToSelect(): JSX.Element[] {
@@ -41,7 +121,10 @@ export class MonthPicker extends React.Component<MonthPickerProps> {
       months.push(
         // i can safely use i here because the months and order are static
         // https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js/43892905#43892905
-        <div key={i} className={`month-picker-month m-1 d-flex align-items-center justify-content-center ${selectedClass}`}>
+        <div
+          key={i}
+          className={`month-picker-month m-1 d-flex align-items-center justify-content-center ${selectedClass}`}
+        >
           {monthDisplay}
         </div>
       );
