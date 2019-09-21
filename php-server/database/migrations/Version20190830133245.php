4017f6c9749d84a67b8c7221695ba7996912229f
Timo Hermans <tmhermans@gmail.com>
2019-09-21 22:45:23 +0200

    make the migrations work for a new project

--- php-server/database/migrations/Version20190830133245.php
+++ php-server/database/migrations/Version20190830133245.php
@@ -15,6 +15,7 @@ class Version20190830133245 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE transactions ADD payee VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE transactions ADD category_id VARCHAR(255) NOT NULL');
     }
 
     /**
