cc7b8656c29bbd146cc7afbb182c37b9a2d0de01
Timo Hermans <tmhermans@gmail.com>
2020-03-09 13:31:05 +0100

    Add react router

--- ruby-react/src/index.js
+++ ruby-react/src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./app/App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
-import {store} from "./app/store";
+import { store } from "./app/store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
