2e77ffd6091b3375c19e08ae63c0649419f9bb21
timo <tmhermans@gmail.com>
2020-03-23 10:45:42 +0100

    make sure tests work again

--- ruby-react/src/setupTests.js
+++ ruby-react/src/setupTests.js
@@ -3,14 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+import useMeasure from "react-use-measure";
+
+jest.mock("react-use-measure");
 
 beforeEach(() => {
   jest.spyOn(global, "fetch").mockImplementation((...args) => {
     console.warn("window.fetch is not mocked for this call", ...args);
     return Promise.reject(new Error("This must be mocked!"));
   });
+  useMeasure.mockImplementation(() => [null, { height: 0 }]);
 });
 
 afterEach(() => {
   global.fetch.mockRestore();
+  useMeasure.mockRestore();
 });
