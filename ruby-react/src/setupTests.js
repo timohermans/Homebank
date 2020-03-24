712effbd41d8935eb8d2bd755c9f63809ce0f450
timo <tmhermans@gmail.com>
2020-03-24 08:25:54 +0100

    Speed up tests
    Disable all animations in tests

--- ruby-react/src/setupTests.js
+++ ruby-react/src/setupTests.js
@@ -2,9 +2,28 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import useMeasure from "react-use-measure";
 
+jest.mock("react-spring", () => ({
+  useTransition: jest
+    .fn()
+    .mockImplementation(isVisible => [{ item: isVisible, key: "1" }]),
+  useChain: jest.fn(),
+  useSpring: jest.fn(),
+  useTrail: jest
+    .fn()
+    .mockImplementation((number, options) => new Array(number)),
+  animated: {
+    path: () => <path data-testid="ANIMATED-COMPONENT" />,
+    div: props => (
+      <div role={props.role} data-testid="ANIMATED-COMPONENT">
+        {props.children}
+      </div>
+    )
+  }
+}));
 jest.mock("react-use-measure");
 
 beforeEach(() => {
