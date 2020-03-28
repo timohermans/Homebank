89e12fdcdb2fdd9e7c27f4aea3463db26904af79
timo <tmhermans@gmail.com>
2020-03-28 14:51:39 +0100

    Make sure the transactionresult is good

--- ruby/app/services/transactions_file_persister.rb
+++ ruby/app/services/transactions_file_persister.rb
@@ -7,16 +7,17 @@ class TransactionsFilePersister
   def initialize(file, parser)
     @file = file
     @parser = parser
+    @transactions_succesful = 0
     @transactions_failed = 0
     @transactions_duplicate = 0
   end
 
   def persist
     extracted_transactions = extract_transactions
-    save_transactions extracted_transactions
+    save extracted_transactions
 
     {
-      amount_successful: extract_transactions.length,
+      amount_successful: @transactions_succesful,
       amount_duplicate: @transactions_duplicate,
       amount_faulty: @transactions_failed
     }
@@ -27,7 +28,7 @@ class TransactionsFilePersister
   def extract_transactions
     transactions = []
     CSV.foreach @file.open, headers: true do |row|
-      transactions << (@parser.parse row)
+      transactions << @parser.parse(row)
     rescue TypeError
       @transactions_failed += 1
     end
@@ -35,16 +36,22 @@ class TransactionsFilePersister
     transactions
   end
 
-  def save_transactions(transactions)
+  def save(transactions)
     transactions.each do |transaction|
-      if transaction.invalid?
-        if transaction.errors[:code].include? 'has already been taken'
-          @transactions_duplicate += 1
-        else
-          @transactions_failed += 1
-        end
+      if transaction.valid?
+        @transactions_succesful += 1
+        transaction.save
+      else
+        determine_invalid_transaction_type transaction
       end
-      transaction.save
+    end
+  end
+
+  def determine_invalid_transaction_type(transaction)
+    if transaction.errors[:code].include? 'has already been taken'
+      @transactions_duplicate += 1
+    else
+      @transactions_failed += 1
     end
   end
 end
