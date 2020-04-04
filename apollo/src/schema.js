876c8ef0d8b89c48195f00e069ef95d71b53582b
Timo Hermans <tmhermans@gmail.com>
2020-04-04 15:37:49 +0200

    Add apollo server to project

---
+++ apollo/src/schema.js
@@ -0,0 +1,47 @@
+const typeDefs = gql`
+    # Your schema will go here
+    scalar Date
+    scalar Upload
+
+    type Query {
+        categories: [Category!]!
+        transactions: [Transaction!]!
+        me: User
+    }
+
+    type Mutation {
+        createCategory(name: String!): Category
+        uploadTransactions(file: Upload!): TransactionUploadResponse
+        login(email: String): String
+    }
+    
+    type Category {
+        id: String!
+        name: String!
+    }
+
+    type Transaction {
+        id: String!
+        date: Date!
+        memo: String!
+        payee: String!
+        inflow: String!
+        outflow: String!
+        isBankTransaction: Boolean!
+        isInflowForBudgeting: Boolean!
+        category: Category
+    }
+    
+    type TransactionUploadResponse {
+        amountSuccessful: Int
+        amountDuplicate: Int
+        amountFaulty: Int
+    }
+    
+    type User {
+        id: ID!
+        email: String!
+    }
+`;
+
+module.exports = typeDefs;
